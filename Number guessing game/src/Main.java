9766f4627d501b1714f99534df6881c5982364fc
ScottStoltz9 <77760802+ScottStoltz9@users.noreply.github.com>
2022-10-09 12:20:19 -0700

    Add files via upload

---
+++ Number guessing game/src/Main.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Main {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        GuessingNumberGame();
+    }
+
+    public static void GuessingNumberGame() {
+        int numberOfTriesLeft = 5;
+        int randomNumber = 1 + (int) (100 * Math.random());
+        System.out.println(randomNumber);
+        System.out.println("Number will be from 1 to 100");
+
+        for (int i = 0; i < numberOfTriesLeft; i++) {
+            System.out.print("Enter your guess: ");
+            int userGuess = scanner.nextInt();
+
+            if (userGuess == randomNumber) {
+                System.out.println("Congrats you guessed correctly");
+                break;
+
+            } else if (userGuess > randomNumber && i != numberOfTriesLeft - 1) {
+                System.out.println("The number is lower than " + userGuess);
+
+            } else if (userGuess < randomNumber && i != numberOfTriesLeft - 1) {
+                System.out.println("The number is greater than " + userGuess);
+            }
+            else {
+                System.out.println("Ran out of tries");
+                System.out.println("Do you want to try again?");
+                System.out.println("Y/N");
+                String answer = scanner.next();
+                if(answer.equalsIgnoreCase("y")) {
+                    GuessingNumberGame();
+                }
+                else {
+                    break;
+                }
+            }
+        }
+    }
+}
